64181af9ed6f3bf0a228d2812a1e36351b29903c
phansonloc1999 <phansonloc1999@gmail.com>
2020-05-16 22:31:34 +0700

    Add basic enemy, eating & growing mechanics

--- WGJ 99.9% Disinfectant/Assets/Scripts/PlayerGrowth.cs
+++ WGJ 99.9% Disinfectant/Assets/Scripts/PlayerGrowth.cs
@@ -2,23 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class PlayerGrowth : MonoBehaviour
 {
 	[SerializeField]
 	private float growthRate = 1.0f;
 	
+	[SerializeField]
+	private bool isGrowing = false;
+	private Vector3 targetScale;
+	[SerializeField]
+	private float growthSpeed = 1.0f;
+	
     // Start is called before the first frame update
     void Start()
     {
+    	
     }
 
     // Update is called once per frame
     void Update()
     {
+    	if (isGrowing)
+    	{
+    		Vector3 newScale = new Vector3(transform.localScale.x + growthSpeed * Time.deltaTime, transform.localScale.y + growthSpeed * Time.deltaTime, 1);
+    		transform.localScale = newScale;
+    		
+    		if (transform.localScale.x >= targetScale.x && transform.localScale.y >= targetScale.y)
+    		{
+    			isGrowing = false;
+    			transform.localScale = targetScale;
+    		}
+    	}
     }
     
     void grow()
     {
-    	transform.localScale = new Vector3(transform.localScale.x * growthRate, transform.localScale.y * growthRate, 1);
+    	isGrowing = true;
+    	targetScale = new Vector3(transform.localScale.x * growthRate, transform.localScale.y * growthRate, 1);
+    }
+    
+    void OnTriggerEnter2D(Collider2D col)
+    {
+    	grow();
+    	Destroy(col.gameObject);
     }
 }
