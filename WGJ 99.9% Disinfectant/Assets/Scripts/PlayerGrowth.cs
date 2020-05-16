b3262e7e801a43186afea71fffe744f994ccbbc6
phansonloc1999 <phansonloc1999@gmail.com>
2020-05-16 20:09:04 +0700

    Player grows by scaling

---
+++ WGJ 99.9% Disinfectant/Assets/Scripts/PlayerGrowth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGrowth : MonoBehaviour
+{
+	[SerializeField]
+	private float growthRate = 1.0f;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    
+    void grow()
+    {
+    	transform.localScale = new Vector3(transform.localScale.x * growthRate, transform.localScale.y * growthRate, 1);
+    }
+}
