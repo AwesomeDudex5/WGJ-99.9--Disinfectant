b7826afeb6ca1537bf31bf4cfd7c1f68e7786133
AwesomeDudex5 <lowdata114@gmail.com>
2020-05-16 00:35:01 -0700

    Player Movement

---
+++ WGJ 99.9% Disinfectant/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Camera mainCamera;
+    public float moveSpeed;
+    private Vector3 mousePosition;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mainCamera.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, -10f);
+        mousePosition = Input.mousePosition;
+
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        mousePosition.z = 1f;
+        this.transform.position = Vector3.Lerp(this.transform.position, mainCamera.ScreenToWorldPoint(mousePosition), moveSpeed / this.transform.localScale.x * Time.deltaTime);
+    }
+}
