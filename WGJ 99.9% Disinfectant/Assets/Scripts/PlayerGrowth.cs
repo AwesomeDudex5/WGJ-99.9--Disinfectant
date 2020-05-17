0d0f5744bd154e6fb4ce4d2b93f43dee0a87f33a
phansonloc1999 <phansonloc1999@gmail.com>
2020-05-17 08:58:18 +0700

    Player grows within duration

--- WGJ 99.9% Disinfectant/Assets/Scripts/PlayerGrowth.cs
+++ WGJ 99.9% Disinfectant/Assets/Scripts/PlayerGrowth.cs
@@ -6,18 +6,19 @@ using UnityEngine;
 public class PlayerGrowth : MonoBehaviour
 {
 	[SerializeField]
-	private float growthRate = 1.0f;
+	private float growthScaleMultiplier = 1.0f;
 	
 	[SerializeField]
 	private bool isGrowing = false;
 	private Vector3 targetScale;
-	[SerializeField]
-	private float growthSpeed = 1.0f;
+	private Transform spriteTransform;
+	private const float GROWTH_DURATION = 2.0f;
+	private float growthSpeed;
 	
     // Start is called before the first frame update
     void Start()
     {
-    	
+    	spriteTransform = transform.GetChild(0).GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -25,26 +26,29 @@ public class PlayerGrowth : MonoBehaviour
     {
     	if (isGrowing)
     	{
-    		Vector3 newScale = new Vector3(transform.localScale.x + growthSpeed * Time.deltaTime, transform.localScale.y + growthSpeed * Time.deltaTime, 1);
-    		transform.localScale = newScale;
+    		Vector3 newScale = new Vector3(spriteTransform.localScale.x + growthSpeed * Time.deltaTime, spriteTransform.localScale.y + growthSpeed * Time.deltaTime, 1);
+    		spriteTransform.localScale = newScale;
     		
-    		if (transform.localScale.x >= targetScale.x && transform.localScale.y >= targetScale.y)
+    		if (spriteTransform.localScale.x >= targetScale.x && spriteTransform.localScale.y >= targetScale.y)
     		{
     			isGrowing = false;
-    			transform.localScale = targetScale;
+    			spriteTransform.localScale = targetScale;
     		}
     	}
     }
     
-    void grow()
+    void eatAndGrow()
     {
     	isGrowing = true;
-    	targetScale = new Vector3(transform.localScale.x * growthRate, transform.localScale.y * growthRate, 1);
+    	targetScale = new Vector3(spriteTransform.localScale.x * growthScaleMultiplier, spriteTransform.localScale.y * growthScaleMultiplier, 1);
+    	growthSpeed = (targetScale.x - spriteTransform.localScale.x) / GROWTH_DURATION;
     }
     
     void OnTriggerEnter2D(Collider2D col)
     {
-    	grow();
+    	Transform other = col.transform.GetChild(0).GetComponent<Transform>();
+    	if (spriteTransform.localScale.x > other.localScale.x && spriteTransform.localScale.y > other.localScale.y)
+    		eatAndGrow();
     	Destroy(col.gameObject);
     }
 }
