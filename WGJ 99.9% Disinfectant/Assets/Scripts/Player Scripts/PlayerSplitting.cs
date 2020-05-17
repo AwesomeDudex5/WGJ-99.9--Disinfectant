7aabbeaeafc53c8237561e625982e774420a8325
AwesomeDudex5 <lowdata114@gmail.com>
2020-05-17 16:35:17 -0700

    Updated Player Split
    -modified player split so it shoots the player out in direction towards mouse
    -Changed player prefab to basic germ
    -Made prefabs for germs, cells, and antibodies

---
+++ WGJ 99.9% Disinfectant/Assets/Scripts/Player Scripts/PlayerSplitting.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSplitting : MonoBehaviour
+{
+    public GameObject germPrefab;
+    [SerializeField] private float splitForceAmount;
+
+    private Rigidbody2D rb;
+    private Vector2 mousePos;
+
+    [SerializeField]
+    private float splitScaleDivider = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetKeyDown(KeyCode.Space) && !GetComponent<PlayerGrowth>().isGrowing)
+        {
+            split();
+            //transform.localScale = new Vector3(transform.localScale.x / splitScaleDivider, transform.localScale.y / splitScaleDivider, 1);
+            //Instantiate(playerSpritePrefab, transform.position, transform.rotation, transform);
+        }
+    }
+
+    void split()
+    {
+        transform.localScale = new Vector3(transform.localScale.x / splitScaleDivider, transform.localScale.y / splitScaleDivider, 1);
+        GameObject otherGerm = Instantiate(germPrefab, transform.position, Quaternion.identity);
+        otherGerm.transform.localScale = this.transform.localScale;
+
+        //Get split facing direction
+        Vector2 lookDirection = mousePos - new Vector2(transform.position.x, transform.position.y);
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        //add force to player object
+        rb.AddForce(transform.up * splitForceAmount, ForceMode2D.Impulse);
+        transform.rotation = Quaternion.identity;
+    }
+}
